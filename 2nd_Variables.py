1164e0ddbc94122212ca80f4c83410f08b333b93
SoyMakko <programacionmakko@gmail.com>
2023-12-13 09:40:17 -0600

    Data Types

---
+++ 2nd_Variables.py
@@ -0,0 +1,24 @@
+"""
+Se puede cambiar el tipado de una variable dependiendo de el contexto en el que se esté utilizando la misma a la hora de estar programando.
+
+Repasar el tipado estático y dinámico de una variable 
+
+
+"""
+
+n = 10
+n = "Hola"
+n = 11.5
+
+# No errors because of what I have explained early on this doc.
+
+"""
+Strings
+Ints
+Booleans
+Tuplas
+Lists
+Diccionarios
+
+Solo se repasarán tuplas, listas y diccionarios ya que las demás ya se dominan correctamente
+"""
